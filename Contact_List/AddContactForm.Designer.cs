c239bc3f7051f70a2656514d71b0baa38289ff26
VanVien <vien11052004@gmail.com>
2025-03-26 16:45:07 +0700

    Sua doi AddContactForm

--- Contact_List/AddContactForm.Designer.cs
+++ Contact_List/AddContactForm.Designer.cs
@@ -31,11 +31,12 @@
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(258, 201);
+            this.button1.Location = new System.Drawing.Point(258, 176);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(189, 33);
             this.button1.TabIndex = 0;
@@ -59,11 +60,21 @@
             this.textBox2.TabIndex = 2;
             this.textBox2.Text = "Số Điện Thoại";
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(293, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Thông tin bổ sung";
+            // 
             // AddContactForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(876, 473);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
@@ -79,5 +90,6 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
     }
 }
